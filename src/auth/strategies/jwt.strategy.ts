ad8125edcbff5434b5464e4517f2b59919bdebf2
Talles Corradini <tallescorradini@gmail.com>
2022-08-24 15:29:35 -0300

    feat: add controllers

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+import { User } from 'src/users/models/users.model';
+import { AuthService } from '../auth.service';
+import { JwtPayload } from '../models/jwt-payload.model';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: authService.getJwtToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(jwtPayload: JwtPayload): Promise<User> {
+    const user = await this.authService.validateUser(jwtPayload);
+
+    if (!user) throw new UnauthorizedException();
+
+    return user;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { SignInDto } from './dtos/signin.dto';
+import { SignupDto } from './dtos/signup.dto';
+import { User } from './models/users.model';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  public async singup(@Body() signupDto: SignupDto): Promise<User> {
+    return this.usersService.signup(signupDto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  public async signin(
+    @Body() signinDto: SignInDto,
+  ): Promise<{ name: string; jwtToken: string; email: string }> {
+    return this.usersService.signin(signinDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  public async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
